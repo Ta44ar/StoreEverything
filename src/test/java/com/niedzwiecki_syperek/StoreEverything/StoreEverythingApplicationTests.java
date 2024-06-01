65874312b46a0f10ed38e7ff7621c63f62ed5c36
misiegn <niemich@wp.pl>
2024-06-01 14:17:33 +0200

    first commit

---
+++ src/test/java/com/niedzwiecki_syperek/StoreEverything/StoreEverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niedzwiecki_syperek.StoreEverything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreEverythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
